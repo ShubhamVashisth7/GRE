c719814cebf1e9c508c669297a916f82bf64f9ce
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2023-11-02 16:40:45 -0400

    add exp config for WO workload

---
+++ script/throughput_wo.sh
@@ -0,0 +1,25 @@
+num_iterations=3
+threads=(36 24 16 8 4 2 1) 
+datasets=("covid")
+index_techniques=("alexol" "lippol" "xindex" "finedex" "artolc" "btreeolc" "masstree" "wormhole_u64")
+
+for dataset in "${datasets[@]}"
+do
+    for thread_num in "${threads[@]}"
+    do
+        for index_technique in "${index_techniques[@]}"
+        do
+            for ((i=1; i<=$num_iterations; i++))
+            do
+                echo "iteration: $i, dataset: $dataset, thread_num: $thread_num, index_technique: $index_technique"
+                ./build/microbench \
+                --keys_file=./datasets/$dataset \
+                --keys_file_type=binary \
+                --read=0.0 --insert=1.0 \
+                --thread_num=$thread_num \
+                --index=$index_technique
+            done
+        done
+    done
+done
+
